59f82faec12d8d9c8c87a6151f33194cbeb49e15
bitplanes <kizoyunlarigithub@gmail.com>
2026-01-30 12:24:56 +0530

    Update ads.js

--- assets/ads.js
+++ assets/ads.js
@@ -6,5 +6,5 @@
     'width' : 300,
     'params' : {}
   };
-</script>
-<script src="https://stemboastfulrattle.com/69a76a5b850adf14193e0bc98b8e1a40/invoke.js"></script>
+<\/script>
+<script src="https://stemboastfulrattle.com/69a76a5b850adf14193e0bc98b8e1a40/invoke.js"><\/script>
