96a47525e00c629d40c0ee7c81628b2e69d05010
bitplanes <kizoyunlarigithub@gmail.com>
2026-01-30 12:16:51 +0530

    Create ads.js

---
+++ assets/ads.js
@@ -0,0 +1,12 @@
+document.write(`
+<script>
+  atOptions = {
+    'key' : '69a76a5b850adf14193e0bc98b8e1a40',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+<\/script>
+<script src="https://stemboastfulrattle.com/69a76a5b850adf14193e0bc98b8e1a40/invoke.js"><\/script>
+`);
